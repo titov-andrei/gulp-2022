7b7c44542ffe6d93d0fc0c2aeadca736a5ed7da2
titov-andrei <titov_andrei@mail.ru>
2021-12-23 12:04:56 +0300

    Update path.js

---
+++ gulp/config/path.js
@@ -0,0 +1,6 @@
+// Получаем имя папки проекта
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const buildFolder = `./dist`;
+const srcFolder = `./src`;
